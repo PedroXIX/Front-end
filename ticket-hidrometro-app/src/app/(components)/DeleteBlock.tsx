2ff1c0aa9b7931ae3379f82fdf1dfc1f561368e0
Fernando Silva <fernando241204@gmail.com>
2024-11-22 21:42:44 -0300

    Commit Tela Ticket
    Tela de Criação de Tickets criada

--- ticket-hidrometro-app/src/app/(components)/DeleteBlock.tsx
+++ ticket-hidrometro-app/src/app/(components)/DeleteBlock.tsx
@@ -1,5 +1,6 @@
 import { faX } from "@fortawesome/free-solid-svg-icons"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import React from "react"
 
 const DeleteBlock = () => {
   return (
